becebe76d27be3dbb3d30627009fc784775b7721
Ricardo Vasquez <ricardovasquez@Ricardos-MacBook-Air.local>
2022-06-26 22:05:08 -0500

    fix render

--- src/pages/baseAuth/login/index.tsx
+++ src/pages/baseAuth/login/index.tsx
@@ -2,19 +2,33 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import store from "../../redux/store";
 import { onLogin, onLogout } from "../../redux/auth/authAction";
+import { useEffect, useState } from "react";
 
 export function LoginPage (props: any){
-    //let navigate = useNavigate();
-    //const authUser = useSelector((x : any) => x);
+    let navigate = useNavigate();
+    const [loggedIn, setloggedIn] = useState(false);
     const dispatch = useDispatch();
 
     store.subscribe(() => {
-        console.log( store.getState() );
+        var storeLoggedIn = store.getState().auth.isLoggedIn;
+        console.log(storeLoggedIn);
+        setloggedIn(storeLoggedIn);
     });
 
-    /*const goToRegister = () => {
+    useEffect(() => {
+        // This gets called after every render, by default
+        // (the first one, and every one after that)
+        console.log('render!');
+    
+        // If you want to implement componentWillUnmount,
+        // return a function from here, and React will call
+        // it prior to unmounting.
+        return () => console.log('unmounting...');
+      })
+
+    const goToRegister = () => {
         navigate("/auth/register");
-    }*/
+    }
 
     const login = () => {
         dispatch(onLogin());
@@ -28,9 +42,11 @@ export function LoginPage (props: any){
         <>
             <h3>
                 Login Page
+                {loggedIn.toString()}
             </h3>
             <button onClick={login}>Login Action</button>
             <button onClick={logout}>Logout Action</button>
+            <button onClick={goToRegister}>GoToRegister</button>
         </>
     );
 }
